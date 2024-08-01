d4673f8bb691b23781666818a6076ec304159479
Felipe Barbedo <felipebarbedo54@gmail.com>
2024-08-01 09:49:46 -0300

    Adding WeatherApp backend

--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -1,2 +1,43 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.net.HttpURLConnection;
+
 public class WeatherApp {
+
+    public static JSONObject getWeatherData(String locationName) {
+        JSONArray locationData = getLocationData(locationName);
+
+        return new JSONObject();
+    }
+
+    public static JSONArray getLocationData(String locationName) {
+
+        locationName = locationName.replace(" ", "+");
+
+        String urlString = "https://geocoding-api.open-meteo.com/v1/search?name=" +
+                locationName + "&count=10&language=en&format=json";
+
+        /*
+        try {
+
+            HttpURLConnection com = fetchApiResponse(urlString);
+        } catch (Exception e) {
+            System.out.println("Error");
+            //e.printStackTrace();
+        }
+
+        return new JSONArray();
+    }
+    */
+
+    /*
+    private static HttpURLConnection fetchApiResponse(String urlString) {
+
+        return new HttpURLConnection();
+    }
+
+     */
+        return new JSONArray();
+    }
 }
